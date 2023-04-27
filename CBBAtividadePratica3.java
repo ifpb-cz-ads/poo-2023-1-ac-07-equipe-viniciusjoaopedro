fbba1b4e12f1ce6a8f22b936fb2a283eec7209da
Vinícius da Silva Ribeiro <vinic.s.ribeiro@gmail.com>
2023-04-27 02:43:14 -0300

    Melhorando a resposta CBBAtividadePratica3

--- CBBAtividadePratica3.java
+++ CBBAtividadePratica3.java
@@ -6,19 +6,15 @@ em vez de definir explicitamente o intervalo do looping,
 utilize o atributo lenght do array para definir seu tamanho. */
 
 public class CBBAtividadePratica3 {
-	public static void main(String[] args) {
-		
-		// Cria o array com capacidade para 100 inteiros
-	    int[] array = new int[100]; 
+    public static void main(String[] args) {
 
-	    // Inicializa cada elemento do array com o número correspondente
-	    for (int i = 0; i < array.length; i++) {
-        array[i] = i;
-	    }
-	    
-	    // Imprime cada número do array
-	    for (int i = 0; i < array.length; i++) {
-        System.out.println(array[i]);
-	    }
-	}
+        // Cria o array com capacidade para 100 inteiros
+        int[] array = new int[100]; 
+
+        // Inicializa e imprime cada elemento do array com o número correspondente
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+            System.out.println(array[i]);
+        }	    	    
+    }
 }
