a8576f3b132464b67743ecc2b7640f57df11762d
Vinícius da Silva Ribeiro <vinic.s.ribeiro@gmail.com>
2023-04-27 02:00:51 -0300

    Adicionada a resposta CBBAtividadePratica2

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,26 @@
+/* Implemente e execute uma nova classe que construa um array 
+ * com capacidade de armazenar 100 inteiros, mas dessa vez 
+ * inicialize cada elemento do array com os números 
+ * de 1 até 100 e imprima-os por meio de uma estrutura for,
+conforme foi exemplifi cado. */
+
+public class CBBAtividadePratica2 {
+
+	public static void main(String[] args) {
+		
+		// Cria o array com capacidade para 100 inteiros
+	    int[] array = new int[100]; 
+
+	    // Inicializa cada elemento do array com o número correspondente
+	    for (int i = 0; i < array.length; i++) {
+        array[i] = i+1;
+	    }
+
+	    // Imprime cada número do array
+	    for (int i = 0; i < array.length; i++) {
+        System.out.println(array[i]);
+        }
+
+	}
+
+}
